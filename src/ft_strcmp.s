708a7ad69ae009d6e3b752a2cd10800aa70f4ced
Denny Cerritos portillo <dcerrito@c2r8s3.42madrid.com>
2022-10-01 06:05:07 +0200

    refactor(ft_strcmp.s): better comment readability

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -3,18 +3,24 @@ global _ft_strcmp
 section .text
   _ft_strcmp:
     mov rax, 0
-    cmp rsi, rdi ; optimistically evaluate if strings[0] are eq.
+    cmp rsi, rdi
+    ; optimistically evaluate if strings[0] are equal
     je terminate
   iter_over_args:
-    mov al, [rdi] ; load lower 8bits from rdi
-    mov bl, [rsi] ; load lower 8bits from rsi
-    inc rdi ; increment *rdi pointer
-    inc rsi ; increment *rsi pointer
-    cmp al, bl ; compare and iterate until byte(`al != bl`)
-    je iter_over_args
+    ; load lower 8bits from rdi, rsi
+    mov al, [rdi]
+    mov bl, [rsi]
+    ; increment *rdi, *rsi pointers
+    inc rdi
+    inc rsi
     cmp al, bl
-    jge skip_and_substr ; if `bl >= al` skip next line
-    sub rax, 256 ; else subtract 256 to rax
+    ; compare and iterate until `al != bl`
+    je iter_over_args
+    ; if `bl >= al` skip next line
+    cmp al, bl
+    jge skip_and_substr
+    ; else subtract 256 to rax
+    sub rax, 256
   skip_and_substr:
     sub al, bl
   terminate:
