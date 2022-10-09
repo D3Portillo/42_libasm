4f10934794935e0602798f98016fbc50a141c2ef
Denny Cerritos portillo <dcerrito@c2r2s5.42madrid.com>
2022-10-10 00:08:09 +0200

    feat(read.s): add clang#read function implementation

--- src/ft_read.s
+++ src/ft_read.s
@@ -1,9 +1,20 @@
 global _ft_read
+extern ___error
 
-; fn(%rdi, %rsi, %rdx)
-; - %rdi File Descriptor
-; - %rsi Writable buffer, destination
+; @see https://opensource.apple.com/source/xnu/xnu-2782.20.48/bsd/kern/syscalls.master
+READ equ 0x2000003
+
+; read(%rdi, %rsi, %rdx)
+; - %rdi File Descriptor to fetch data from
+; - %rsi Writable buffer. Destination
 ; - %rdx N bytes to read from %rdi
 section .text
   _ft_read:
+    mov rax, READ
+    ; invoke read()
+    syscall
+    jnc terminate ; terminate if no carry flag
+    call ___error
+    mov rax, -1; if a flag is set we update return val to be -1
+  terminate:
     ret
