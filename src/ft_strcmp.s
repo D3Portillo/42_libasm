9e0828ca96d5b46545f963a9469ccc41076e03c1
Denny Cerritos portillo <dcerrito@c2r8s3.42madrid.com>
2022-10-10 21:45:30 +0200

    refactor(ft_strcmp.s): remove unused code logic and harden for unsigned conditions

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -7,7 +7,7 @@ section .text
     ; optimistically evaluate if strings[0] are equal
     je terminate
   iter_over_args:
-    ; load lower 8bits from rdi, rsi
+    ; byte load from rdi, rsi to 8-bit registers
     mov al, [rdi]
     mov bl, [rsi]
     ; increment *rdi, *rsi pointers
@@ -16,9 +16,8 @@ section .text
     cmp al, bl
     ; compare and iterate until `al != bl`
     je iter_over_args
+    ja skip_and_substr
     ; if `bl >= al` skip next line
-    cmp al, bl
-    jge skip_and_substr
     ; else subtract 256 to rax
     sub rax, 256
   skip_and_substr:
