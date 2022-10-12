e05b3eda139205acd4cc048c738b8e02735668e8
Denny Cerritos portillo <dcerrito@c2r4s3.42madrid.com>
2022-10-12 23:22:29 +0200

    chores(ft_read.s): store rax in stack instead of asuming rbx is existent.

--- src/ft_read.s
+++ src/ft_read.s
@@ -12,10 +12,11 @@ section .text
     ; terminate if no carry flag
     jnc terminate
     ; save `errno` before calling ___error
-    mov rbx, rax
+    push rax
     call ___error
     ; get errno global variable pointer and store it in rax.
     ; @see https://github.com/cacharle/libasm_test/issues/2#issuecomment-622371402
+    pop rbx
     mov [rax], rbx
     ; set pointer value for errno to be `syscall` return value from READ
     mov rax, -1
