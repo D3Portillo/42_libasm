097a0dedb591d6c130f7887e67a1572cca0b64b4
Denny Cerritos portillo <dcerrito@c2r4s3.42madrid.com>
2022-10-12 23:26:57 +0200

    fix(ft_write.s): store write syscall rax value in stack to keep it among function calls

--- src/ft_write.s
+++ src/ft_write.s
@@ -1,5 +1,19 @@
 global _ft_write
+extern ___error
 
+; @see https://opensource.apple.com/source/xnu/xnu-2782.20.48/bsd/kern/syscalls.master
+WRITE equ 0x2000004
+
+; write(int fd, void *buff, size_t limit);
 section .text
   _ft_write:
+    mov rax, WRITE
+    syscall
+    jnc terminate
+    push rax
+    call ___error
+    pop rbx
+    mov [rax], rbx
+    mov rax, -1
+  terminate:
     ret
