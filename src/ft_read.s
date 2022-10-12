560ba689eb6fcc3848caa5c018f950b9ff729f97
Denny Cerritos portillo <dcerrito@c2r8s5.42madrid.com>
2022-10-12 03:51:59 +0200

    feat(ft_read.s): add functional implementation with the actual unistd::read() behaviour

--- src/ft_read.s
+++ src/ft_read.s
@@ -4,17 +4,21 @@ extern ___error
 ; @see https://opensource.apple.com/source/xnu/xnu-2782.20.48/bsd/kern/syscalls.master
 READ equ 0x2000003
 
-; read(%rdi, %rsi, %rdx)
-; - %rdi File Descriptor to fetch data from
-; - %rsi Writable buffer. Destination
-; - %rdx N bytes to read from %rdi
 section .text
   _ft_read:
     mov rax, READ
     ; invoke read()
     syscall
-    jnc terminate ; terminate if no carry flag
+    ; terminate if no carry flag
+    jnc terminate
+    ; save `errno` before calling ___error
+    mov rbx, rax
     call ___error
-    mov rax, -1; if a flag is set we update return val to be -1
+    ; get errno global variable pointer and store it in rax.
+    ; @see https://github.com/cacharle/libasm_test/issues/2#issuecomment-622371402
+    mov [rax], rbx
+    ; set pointer value for errno to be `syscall` return value from READ
+    mov rax, -1
+    ; return error status code as expected for read implementation
   terminate:
     ret
